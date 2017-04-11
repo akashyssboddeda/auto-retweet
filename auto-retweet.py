613d9f00adbbcabdf2cae85575f34d0fabd830ad
Crudités <crudites.jambon@gmail.com>
2017-04-11 18:38:36 +0200

    Tweet about news articles as well

--- auto-retweet.py
+++ auto-retweet.py
@@ -29,10 +29,17 @@ class Bot(discord.Client):
                     print('  \033[1m@%s\033[0;92m\n%s\033[0m\n' % (status.user.name, text))
             except Exception as e:
                 print('\n\033[1;31m%s: %s\033[0m\n' % (type(e).__name__, e))
+        elif msg.channel.id == '256392899720249344':
+            for link in re.findall(r'https?://\S+', msg.content):
+                try:
+                    self.api.PostUpdate(link)
+                    print('\033[36m%s\033[0m\n' % link)
+                except Exception as e:
+                    print('\n\033[1;31m%s: %s\033[0m\n' % (type(e).__name__, e))
 
     async def on_ready(self):
         for channel in self.get_all_channels():
-            if channel.id == '293416436620197889':
+            if channel.id in ('293416436620197889', '256392899720249344'):
                 async for msg in self.logs_from(channel, limit=16):
                     self.retweet(msg)
     async def on_message(self, msg):
